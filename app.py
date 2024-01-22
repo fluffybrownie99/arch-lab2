6cad0709f338f784188681da2ca8c79f095cf480
Ramith <ramithr11@gmail.com>
2024-01-22 09:35:26 -0800

    prep for lab 3

--- app.py
+++ app.py
@@ -2,29 +2,8 @@ import connexion
 from connexion import NoContent
 import datetime
 import json
+from update_event_data import update_event_data
 
-
-def update_event_data(event_type, event_data):
-    EVENTFILE = "events.json"
-    MAX_EVENTS = 5
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-    formatted_data = ", ".join([f"{key}: {event_data[key]}" for key in event_data])
-    event = {
-        "received_timestamp": current_time,
-        "msg_data": formatted_data
-    }
-    try:
-        with open( EVENTFILE, "r") as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        data = {}
-    if event_type not in data:
-        data[event_type] = {"count":0, "events":[]}
-    data[event_type]["count"] += 1
-    data[event_type]["events"].insert(0, event)
-    data[event_type]["events"] = data[event_type]["events"][:MAX_EVENTS]
-    with open(EVENTFILE, "w") as file:
-        json.dump(data, file, indent=4)
 
 
 def media_upload(body, deviceId):
