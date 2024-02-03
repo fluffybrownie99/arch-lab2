d7c10c3f7e02e961383df69e4cb15fff2cfb2cb3
fluffybrownie99 <115048106+fluffybrownie99@users.noreply.github.com>
2024-02-03 13:18:58 -0800

    partway through completion, prior to connecting mysql

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
-import connexion
 from connexion import NoContent
-import datetime
-import json
 from update_event_data import update_event_data
-import requests
+import connexion, requests, yaml, logging, logging.config, datetime, json, uuid
 
-# CONSTS
-DATA_STORAGE = 'http://127.0.0.1:8090/home/media/'
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+logger = logging.getLogger('basicLogger')
+
+# URLs from YAML
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+    log_upload = app_config.get('eventstore1', {}).get('url')
+    log_playback = app_config.get('eventstore2', {}).get('url')
+
 # EndPoints
 
-def media_upload(body, deviceId):
+def media_upload(body):
     # update_event_data("media_upload",body)
+    trace_id = str(uuid.uuid4())
+    logger.info(f'Recieved event "media_upload" request with a trace id of {trace_id}')
+    body['trace_id']=trace_id
     file_path = './test.jpg'
     files = {'file': open(file_path, 'rb')}
-    media_upload_url = DATA_STORAGE + 'upload?deviceId=' + deviceId
-    
+    media_upload_url = app_config["eventstore1"]["url"] 
     response = requests.post(media_upload_url, files=files, data=body)
+    logger.info(f'Returned event "media_upload" response (Id: {trace_id}) with status {response.status_code}')
     return response.json(), response.status_code
+    
 
-
-def media_playback(body, deviceId):
+def media_playback(body):
     # print(f'{body[mediaType]} \n{body[fileSize]} \n{body[uploadTime]} \n')
-    media_playback_url = DATA_STORAGE + 'playback?deviceId=' + deviceId
+    trace_id = str(uuid.uuid4())
+    logger.info(f'Recieved event "media_upload" request with a trace id of {trace_id}')
+    body['trace_id']=trace_id
+    media_playback_url = app_config["eventstore2"]["url"] 
     response = requests.post(media_playback_url, json=body, headers={"Content-Type": "application/json"})
+    logger.info(f'Returned event "media_upload" response (Id: {trace_id}) with status {response.status_code}')
     return NoContent, response.status_code
+
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
@@ -32,7 +46,6 @@ app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yaml",
             strict_validation=True,
             validate_responses=True)
-
 
 #openapi.yaml is the name of the file
 # strict_validation - whether to validate requests parameters or messages
