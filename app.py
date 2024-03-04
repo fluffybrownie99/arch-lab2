e8b1eefbb94b910dc8c24ba794b4aa8f8568dd9d
fluffybrownie99 <115048106+fluffybrownie99@users.noreply.github.com>
2024-03-03 18:11:37 -0800

    got messaging working :)

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from connexion import NoContent
 from update_event_data import update_event_data
+from pykafka import KafkaClient
 import connexion, requests, yaml, logging, logging.config, datetime, json, uuid
 #Log loader
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
+# with open('log_conf.yml', 'r') as f:
+#     log_config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(log_config)
 logger = logging.getLogger('basicLogger')
 
 # URLs from YAML
@@ -12,6 +13,7 @@ with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
     log_upload = app_config.get('eventstore1', {}).get('url')
     log_playback = app_config.get('eventstore2', {}).get('url')
+    
 
 # EndPoints
 
@@ -24,9 +26,21 @@ def media_upload(body):
     file_path = './test.jpg'
     files = {'file': open(file_path, 'rb')}
     media_upload_url = app_config["eventstore1"]["url"] 
-    response = requests.post(media_upload_url, files=files, data=body)
+    # response = requests.post(media_upload_url, files=files, data=body)
+    #Messaging portion
+    kafka_config = app_config['events']
+    client = KafkaClient(hosts=f"{kafka_config['hostname']}:{kafka_config['port']}")
+    topic = client.topics[str.encode(kafka_config['topic'])]
+    producer = topic.get_sync_producer()
+    msg = {"type":"mediaupload",
+            "datetime":datetime.datetime.now().strftime(
+                "%Y-%m-%dT%H:%M:%S"),
+            "payload": body}
+    msg_str = json.dumps(msg)
+    producer.produce(msg_str.encode('utf-8'))
+    
     logger.info(f'Returned event "media_upload" response (Id: {trace_id}) with status {response.status_code}')
-    return response.json(), 201  
+    return json.loads(msg_str), 201  
 
 
 
@@ -36,9 +50,20 @@ def media_playback(body):
     logger.info(f'Recieved event "media_playback" request with a trace id of {trace_id}')
     body['trace_id']=trace_id
     media_playback_url = app_config["eventstore2"]["url"] 
-    response = requests.post(media_playback_url, json=body, headers={"Content-Type": "application/json"})
+    #response = requests.post(media_playback_url, json=body, headers={"Content-Type": "application/json"})
+    # Messaging
+    kafka_config = app_config['events']
+    client = KafkaClient(hosts=f"{kafka_config['hostname']}:{kafka_config['port']}")
+    topic = client.topics[str.encode(kafka_config['topic'])]
+    producer = topic.get_sync_producer()
+    msg = {"type":"mediaplayback",
+            "datetime":datetime.datetime.now().strftime(
+                "%Y-%m-%dT%H:%M:%S"),
+            "payload": body}
+    msg_str = json.dumps(msg)
+    producer.produce(msg_str.encode('utf-8'))
     logger.info(f'Returned event "media_playback" response (Id: {trace_id}) with status {response.status_code}')
-    return response.json(), 201  
+    return json.loads(msg_str), 201  
 
 
 
